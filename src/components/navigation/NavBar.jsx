b141bdc4e41fba35a85701dd5011e7387c6ff38c
Vaibhav Kumar <vaibhav11kr@gmail.com>
2024-03-28 20:01:58 +0530

    70%

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -8,7 +8,7 @@ import {Menu} from "@styled-icons/entypo/Menu"
 
 const NavBar = () => {
   return (
-    <div className="bg-[#1A1E1C] h-[98%] w-[80%] md:w-[20%] pt-6 z-10 shadow-lg flex flex-col rounded-r-lg overflow-scroll mb-4">
+    <div className="bg-[#1A1E1C] h-[98%] w-[70%] md:w-[20%] pt-6 z-10 shadow-lg flex flex-col rounded-r-lg overflow-scroll mb-4">
     <div className="flex mb-6 items-center justify-between w-[97%] h-14">
         <img src={Logo} alt="logo" className="h-12"/>
         <Menu className="text-white h-7 mb-3 self-end"/>
