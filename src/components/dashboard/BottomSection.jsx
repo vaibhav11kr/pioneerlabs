3f7fb43fea0c98bbfcd1bece9b504ef393e590c2
Vaibhav Kumar <vaibhav11kr@gmail.com>
2024-03-30 01:43:00 +0530

    done almost

--- src/components/dashboard/BottomSection.jsx
+++ src/components/dashboard/BottomSection.jsx
@@ -17,23 +17,18 @@ const BottomSection = () => {
       }
       const fetchedData = await response.json();
       const modifiedBpi = {};
-    for (const currencyCode in fetchedData.bpi) {
-      const currencyData = fetchedData.bpi[currencyCode];
-      const intratefloat = currencyData.rate_float;
-      const stsymbol = currencyData.symbol;
-      const intrate = parseFloat(currencyData.rate.replace(',', ''));
-      const percentageChange = (((intrate - intratefloat) / intratefloat) * 100000000).toFixed(2) + '%'; 
-      currencyData.rate = (intrate / 100000).toFixed(2);
-      currencyData.rate_float = percentageChange;
-      modifiedBpi[currencyCode] = currencyData;
-    }
+      for (const currencyCode in fetchedData.bpi) {
+        const currencyData = fetchedData.bpi[currencyCode];
+        const intratefloat = currencyData.rate_float;
+        const stsymbol = currencyData.symbol;
+        const intrate = parseFloat(currencyData.rate.replace(',', ''));
+        const percentageChange = (((intrate - intratefloat) / intratefloat) * 100000000).toFixed(2) + '%'; 
+        currencyData.rate = (intrate / 100000).toFixed(2);
+        currencyData.rate_float = percentageChange;
+        modifiedBpi[currencyCode] = currencyData;
+      }
 
-    // Set the modified data in the state
-    setData({
-      ...fetchedData,
-      bpi: modifiedBpi
-    });
-      setData(Object.values(fetchedData.bpi));
+      setData(Object.values(modifiedBpi));
 
     } catch (error) {
       setError(error);
@@ -49,11 +44,10 @@ const BottomSection = () => {
   return (
     <div className="flex flex-col">
       <p className="text-white text-xl mb-4">Assets</p>
-      <div className="flex flex-row gap-4 overflow-scroll w-full">
+      <div className="flex flex-row gap-4 overflow-x-scroll w-[100%]">
         {!loading ?
-          Array.isArray(data) && data.map((item) => (
-
-            <div className="bg-[#171717] w-48 rounded-lg p-2" key={item.code}>
+          data.map((item) => (
+            <div className="bg-[#171717] min-w-48 h-auto rounded-lg p-2" key={item.code}>
               <div className="flex flex-row items-center mb-3">
                 <DollarCircle className="h-8" />
                 <p className="text-xs ml-2">{item.code}</p>
@@ -62,10 +56,13 @@ const BottomSection = () => {
                 {item.description}
               </div>
               <div className="flex flex-row justify-between items-center mb-3">
-                <p className={`text-lg ${item.symbol}`}>{item.rate} {item.symbol}</p>
-                <p className="text-[10px] text-[#4F4F4F] ">
-  {item.rate_float < 0 ? ('+' + item.rate_float ): item.rate_float} 
+                <p className={`text-lg`}>{item.rate} <span dangerouslySetInnerHTML={{ __html: item.symbol }}></span></p>
+
+                <p className="text-[10px] text-[#4F4F4F]">
+  {item.rate_float.charAt(0) === '-' ? item.rate_float : `+${item.rate_float}`}
 </p>
+
+
               </div>
               <div className="flex flex-row justify-between items-center">
                 <InformationCircle className="text-[#1B9A59] h-6" />
@@ -77,11 +74,41 @@ const BottomSection = () => {
                 </button>
               </div>
             </div>
-            
           ))
           : <p>Loading...</p>
         }
-        
+        {!loading ?
+          data.map((item) => (
+            <div className="bg-[#171717] min-w-48 rounded-lg p-2" key={item.code}>
+              <div className="flex flex-row items-center mb-3">
+                <DollarCircle className="h-8" />
+                <p className="text-xs ml-2">{item.code}</p>
+              </div>
+              <div className="flex flex-row text-[12px] mb-3 leading-4 text-[#4F4F4F]">
+                {item.description}
+              </div>
+              <div className="flex flex-row justify-between items-center mb-3">
+                <p className={`text-lg`}>{item.rate} <span dangerouslySetInnerHTML={{ __html: item.symbol }}></span></p>
+
+                <p className="text-[10px] text-[#4F4F4F]">
+  {item.rate_float.charAt(0) === '-' ? item.rate_float : `+${item.rate_float}`}
+</p>
+
+
+              </div>
+              <div className="flex flex-row justify-between items-center">
+                <InformationCircle className="text-[#1B9A59] h-6" />
+                <button
+                  type="submit"
+                  className="h-7 w-14 p-1 bg-[#1B9A59] text-xs rounded"
+                >
+                  Trade
+                </button>
+              </div>
+            </div>
+          ))
+          : <p>Loading...</p>
+        }
       </div>
     </div>
   );
