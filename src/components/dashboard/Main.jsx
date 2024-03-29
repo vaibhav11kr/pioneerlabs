f860a86c8ecb0757041bb200ef689f65d724602d
Vaibhav Kumar <vaibhav11kr@gmail.com>
2024-03-30 02:26:13 +0530

    bug

--- src/components/dashboard/Main.jsx
+++ src/components/dashboard/Main.jsx
@@ -5,7 +5,7 @@ import BottomSection from './BottomSection';
 const name = "Brooklyn Simmons"
 const Main = () => {
   return (
-    <div className='pl-7 pt-9 text-white text-2xl w-[calc(100vw-2.5rem)] pr-7 h-full'>
+    <div className='pl-7 pt-9 text-white text-2xl w-[calc(100vw-20%)] pr-7 h-full'>
         <div className="mb-8">
             <p className='text-2xl'>Hello, <span className='text-gradient'>{name}</span> 👋🏻</p>
             <p className='text-xl'>Welcome to <span className='text-[#259926]'>Spot trading !</span></p>
