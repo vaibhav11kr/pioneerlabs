b91d2c47e9e02d4911994c8a0841fb28e8363efb
Vaibhav Kumar <vaibhav11kr@gmail.com>
2024-03-30 00:42:37 +0530

    last fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import Main from './components/dashboard/Main';
 import NavBar from './components/navigation/NavBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="bg-[#0B0B0B] w-screen h-screen overflow-scroll flex text-white">
-      <NavBar />
-      <Main />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<Main />} />
+          <Route path="/contact" element={<Main />} />
+          <Route path="/trade" element={<Main />} />
+          <Route path="/history" element={<Main />} />
+          <Route path="/wallet" element={<Main />} />
+          <Route path="*" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/dashboard/MarketOverview.jsx
+++ src/components/dashboard/MarketOverview.jsx
@@ -4,7 +4,7 @@ import PopulationGraph from './PopulationGraph';
 const MarketOverview = () => {
   return (
     <div className='bg-[#171717] h-auto md:h-[90%] md:w-[50%] rounded-xl p-4 mb-8 md:mb-0 overflow-hidden'>
-      <PopulationGraph />
+      {/* <PopulationGraph /> */}
     </div>
   );
 };
