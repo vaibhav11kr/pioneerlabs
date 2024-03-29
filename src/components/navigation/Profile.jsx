9dd136453f581e71781fb6764176e8c7bf45d053
Vaibhav Kumar <vaibhav11kr@gmail.com>
2024-03-30 02:40:31 +0530

    bug

--- src/components/navigation/Profile.jsx
+++ src/components/navigation/Profile.jsx
@@ -3,7 +3,7 @@ import {PersonCircle} from "@styled-icons/bootstrap/PersonCircle"
 import {ThreeDotsVertical} from "@styled-icons/bootstrap/ThreeDotsVertical"
 const Profile = () => {
   return (
-    <div className='bg-[#333333] w-[93%] text-white flex flex-row h-auto pt-5 pb-5 justify-evenly items-center text-xs ml-2 rounded-md'>
+    <div className='bg-[#333333] w-[93%] text-white flex flex-row h-auto pt-5 pb-5 justify-evenly items-center text-xs ml-2 rounded-md mb-8'>
         <div className="h-8">
         <PersonCircle className='h-8 text-[#686868]'/>
             
